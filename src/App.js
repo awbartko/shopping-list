1958d045362cb7e157eeed785e7dea48e582f721
ABartko <awartest@yahoo.com>
2024-05-01 19:56:53 -0400

    fetching data from server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import ShoppingForm from './Components/ShoppingForm/ShoppingForm';
+import ShoppingList from './Components/ShoppingList/ShoppingList';
 
 function App() {
+
+  const [shoppingList, setShoppingList] = useState([]);
+
+  useEffect(() => {
+    fetch("https://2z3tx8-8080.csb.app/api/list")
+      .then(x => x.json())
+      .then(response => {
+        setShoppingList(response);
+      })
+  }, []);
+
+  console.log(shoppingList);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Shopping List</h1>
       </header>
+      <main>
+        <ShoppingForm />
+        <ShoppingList items={shoppingList} />
+      </main>
     </div>
   );
 }
