f14d22377c004127b293d5f13bf88df99e8437ac
ABartko <awartest@yahoo.com>
2024-05-13 18:53:46 -0400

    Updates

---
+++ precache-manifest.f14fcba46a56ded8cd83d8bf55560021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10acfe4ea293169c8e99c8c5e582d1e",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "94cc6a4a95ddd0e72738",
+    "url": "/shopping-list/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "54f1945ea315486f31e0",
+    "url": "/shopping-list/static/css/main.c86e2f91.chunk.css"
+  },
+  {
+    "revision": "94cc6a4a95ddd0e72738",
+    "url": "/shopping-list/static/js/2.d4c0f5a8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/shopping-list/static/js/2.d4c0f5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f1945ea315486f31e0",
+    "url": "/shopping-list/static/js/main.f4f47ebd.chunk.js"
+  },
+  {
+    "revision": "82b5c0c049722db67a99",
+    "url": "/shopping-list/static/js/runtime-main.8ac18670.js"
+  }
+]);
